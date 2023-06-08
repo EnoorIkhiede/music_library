cd7c7f6e8a6afa1f1c28aeae8ba1649e396eea07
Enoor <98205383+EnoorIkhiede@users.noreply.github.com>
2023-06-08 01:55:55 -0600

    Add files via upload

---
+++ library.js
@@ -0,0 +1,150 @@
+const library = {
+  tracks: { t01: { id: "t01",
+                   name: "Code Monkey",
+                   artist: "Jonathan Coulton",
+                   album: "Thing a Week Three" },
+            t02: { id: "t02",
+                   name: "Model View Controller",
+                   artist: "James Dempsey",
+                   album: "WWDC 2003"},
+            t03: { id: "t03",
+                   name: "Four Thirty-Three",
+                   artist: "John Cage",
+                   album: "Woodstock 1952"}
+          },
+  playlists: { p01: { id: "p01",
+                      name: "Coding Music",
+                      tracks: ["t01", "t02"]
+                    },
+               p02: { id: "p02",
+                      name: "Other Playlist",
+                      tracks: ["t03"]
+                    }
+             }
+};
+
+/////////////////////////////
+// FUNCTIONS TO IMPLEMENT:
+/////////////////////////////
+
+// prints a list of all playlists, in the form:
+// p01: Coding Music - 2 tracks
+// p02: Other Playlist - 1 tracks
+const printPlaylists = function() {
+    for (let playlistId in library.playlists) {
+       const playlist = library.playlists[playlistId];
+     console.log(`${playlist.id}: ${playlist.name} - ${playlist.tracks.length} tracks`);
+  }
+};
+
+
+// prints a list of all tracks, using the following format:
+// t01: Code Monkey by Jonathan Coulton (Thing a Week Three)
+// t02: Model View Controller by James Dempsey (WWDC 2003)
+// t03: Four Thirty-Three by John Cage (Woodstock 1952)
+const printTracks = function() {
+   for (let trackId in library.tracks) {
+   const track = library.tracks[trackId];
+         console.log(`${track.id}: ${track.name} by ${track.artist} (${track.album})`);
+   }
+};
+
+
+// prints a list of tracks for a given playlist, using the following format:
+// p01: Coding Music - 2 tracks
+// t01: Code Monkey by Jonathan Coulton (Thing a Week Three)
+// t02: Model View Controller by James Dempsey (WWDC 2003)
+const printPlaylist = function(playlistId) {
+       const playlist = library.playlists[playlistId];
+       console.log(`${playlist.id}: ${playlist.name} - ${playlist.tracks.length} tracks`);
+       for (let trackId of playlist.tracks) {
+         const track = library.tracks[trackId];
+         console.log(`${track.id}: ${track.name} by ${track.artist} (${track.album})`);
+   }  
+};
+
+
+// adds an existing track to an existing playlist
+const addTrackToPlaylist = function(trackId, playlistId) {
+      library.playlists[playlistId].tracks.push(trackId);
+};
+
+
+// generates a unique id
+// (already implemented: use this for addTrack and addPlaylist)
+const generateUid = function() {
+  return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+};
+
+
+// adds a track to the library
+const addTrack = function(name, artist, album) {
+   const trackId = generateUid();
+   library.tracks[trackId] = {
+    id: trackId,
+    name: name,
+    artist: artist,
+    album: album
+  };
+};
+
+
+// adds a playlist to the library
+const addPlaylist = function(name) {
+    const playlistId = generateUid();
+     library.playlists[playlistId] = {
+         id: playlistId,
+         name: name,
+         tracks: []
+    };
+};
+
+
+// STRETCH:
+// given a query string string, prints a list of tracks
+// where the name, artist or album contains the query string (case insensitive)
+// tip: use "string".search("tri") 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/search
+const printSearchResults = function(query) {
+       query = query.toLowerCase();
+  for (let trackId in library.tracks) {
+    const track = library.tracks[trackId];
+    if (
+      track.name.toLowerCase().includes(query) ||
+      track.artist.toLowerCase().includes(query) ||
+      track.album.toLowerCase().includes(query)
+    ) {
+      console.log(`${track.id}: ${track.name} by ${track.artist} (${track.album})`);
+    }
+  }
+};
+
+
+// Testing the implemented functions
+
+console.log("Printing all playlists:");
+printPlaylists();
+
+console.log("\nPrinting all tracks:");
+printTracks();
+
+console.log("\nPrinting playlist with ID p01:");
+printPlaylist("p01");
+
+console.log("\nAdding track t03 to playlist p01:");
+addTrackToPlaylist("t03", "p01");
+console.log("Updated playlist p01:");
+printPlaylist("p01");
+
+console.log("\nAdding a new track:");
+addTrack("New Track", "New Artist", "New Album");
+console.log("All tracks after adding new track:");
+printTracks();
+
+console.log("\nAdding a new playlist:");
+addPlaylist("New Playlist");
+console.log("All playlists after adding new playlist:");
+printPlaylists();
+
+console.log("\nSearching for tracks containing 'Code':");
+printSearchResults("Code");
